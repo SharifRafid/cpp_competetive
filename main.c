ae92822bfc3bc9b0528cefc839001be953285e27
sharifrafid <sharifrafidurrahman@gmail.com>
2024-09-21 16:34:53 +0600

    Solved 2758

--- main.c
+++ main.c
@@ -7,36 +7,33 @@ int main()
     char *buffer = NULL;
     size_t buffSize = 0;
 
-    getline(&buffer, &buffSize, stdin);
-    int x;
-    sscanf(buffer, "%d", &x);
-    int c = 0, r = 0, s = 0;
-    for (int i = 0; i < x; i++)
+    while (1)
     {
-        int d;
         getline(&buffer, &buffSize, stdin);
-        char chr;
-        sscanf(buffer, "%d %c", &d, &chr);
-        if (chr == 'C')
+
+        int n;
+        sscanf(buffer, "%d", &n);
+
+        // if (x == 0)
+        // {
+        //     break;
+        // }
+
+        for (int i = 0; i < n; i++)
         {
-            c += d;
+            int x;
+            getline(&buffer, &buffSize, stdin);
+            sscanf(buffer, "%d", &x);
+            if (x % 2 == 0)
+            {
+                printf("0\n");
+            }else{
+                printf("1\n");
+            }
         }
-        else if (chr == 'R')
-        {
-            r += d;
-        }
-        else if (chr == 'S')
-        {
-            s += d;
-        }
+
+        break;
     }
-    printf("Total: %d cobaias\n", c + r + s);
-    printf("Total de coelhos: %d\n", c);
-    printf("Total de ratos: %d\n", r);
-    printf("Total de sapos: %d\n", s);
-    printf("Percentual de coelhos: %0.2lf %%\n", ((double)c / (double)(c + r + s)) * 100);
-    printf("Percentual de ratos: %0.2lf %%\n", ((double)r / (double)(c + r + s)) * 100);
-    printf("Percentual de sapos: %0.2lf %%\n", ((double)s / (double)(c + r + s)) * 100);
 
     return 0;
 }
