336a904fac8ae7c8155bb3b0df7a8ff93384653f
sharifrafid <sharifrafidurrahman@gmail.com>
2024-11-28 00:47:58 +0600

    fix: takeoff easy preli problems solved

---
+++ bee/1936.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(const int &t)
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    solve(t);
+    // while (t--)
+    // {
+    // }
+    return 0;
+}
